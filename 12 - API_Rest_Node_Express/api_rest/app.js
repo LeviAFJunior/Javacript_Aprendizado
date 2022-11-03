322296e0aed771a1036224aac5e01f768c381fee
Levi <levi.af@gmail.com>
2022-11-03 20:08:39 -0300

    resolvendo erros

--- 12 - API_Rest_Node_Express/api_rest/app.js
+++ 12 - API_Rest_Node_Express/api_rest/app.js
@@ -17,15 +17,14 @@ const whiteList = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if(whiteList.indexOf(origin) !== -1 || !origin) {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
 };
-
 
 class App {
   constructor() {
